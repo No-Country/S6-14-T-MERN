55bc062a094dccfdaeecb31208c12d8f4240cc32
Jared Mejia <jaredmejia4440@gmail.com>
2023-02-07 17:47:24 -0600

    feat: only images are allowed on multer

--- BACK/src/utils/multer.utils.js
+++ BACK/src/utils/multer.utils.js
@@ -1,7 +1,24 @@
 const multer = require("multer");
 
+const boom = require("@hapi/boom");
+
 const storage = multer.memoryStorage();
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file ||
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(boom.badRequest("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+});
 
 module.exports = { upload };
