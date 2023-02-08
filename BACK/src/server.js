3ff1cad92fa31a0e3b2745896bad77ee4864e9b3
David Mamani <106356130+davidmedev222@users.noreply.github.com>
2023-02-07 22:46:58 -0300

    Revert "chore:Iniciar secion"

--- BACK/src/server.js
+++ BACK/src/server.js
@@ -1,29 +1,14 @@
-const express = require("express");
-const { connectDB } = require("./config/config");
-const globalErrorHandler = require("./controllers/error.controller");
-const { boomErrorHandler } = require("./middlewares/error.handler")
+const express = require('express')
 
-const routerApi = require("./routes/index");
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 3000;
+const app = express()
 
-const app = express();
-
-connectDB()
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-routerApi(app);
-
-app.use(boomErrorHandler);
-app.use(globalErrorHandler);
-
-require("./utils/auth");
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Servidor funcionando en el puerto ${port}`)
 });
 
-
-module.exports = app;
+module.exports = app
