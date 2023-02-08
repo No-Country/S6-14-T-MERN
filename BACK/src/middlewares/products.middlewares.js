4deca5c8ef21405ceaf0aa616feb28dcbdddf862
Jared Mejia <jaredmejia4440@gmail.com>
2023-02-08 17:49:27 -0600

    feat: productExist middleware

---
+++ BACK/src/middlewares/products.middlewares.js
@@ -0,0 +1,22 @@
+const productModel = require("../models/products.model");
+
+const boom = require("@hapi/boom");
+
+const productExist = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const product = await productModel.findOne({ _id: id }, { __v: 0 });
+
+    if (!product) {
+      throw boom.notFound("Product Not Found");
+    }
+
+    req.product = product;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { productExist };
