825b1661a0737ca6ce4a2af896444117ff6da679
Sergio Calbiño <serggio.c86@gmail.com>
2023-02-07 22:09:22 -0300

    chore: getAllUsers, getOneUser, CreateUser in progress

---
+++ BACK/src/routes/users.routes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const  Request = require('express');
+const { getUsers, getOneUser, createUser } = require('../controllers/users.controller');
+const { createUserValidators } = require('../middlewares/validators.middlewares');
+const userModel = require('../models/users.model');
+
+
+const usersRouter = express.Router();
+
+usersRouter.get('/all', async (req, res, next) => {
+
+    try {
+        const users = await getUsers()
+        res.status(200).json({status: 'succes', data: { users }})
+        
+    } catch (error) {
+        next(error)
+        
+    }
+
+});
+
+usersRouter.get("/:id", async (req, res, next) => {
+    try {
+        const user = await getOneUser(req)
+        return res.status(200).json({status: 'succes', data: { user }})
+        
+    } catch (error) {
+        next(error)
+        
+    }
+})
+
+usersRouter.post('/create', createUserValidators, async (req, res, next) => {
+    try {
+        const user = await createUser(req)
+        res.status(200).json({ status: 'succes', data: { user } })
+        
+    } catch (error) {
+        next(error)
+        
+    }
+})
+
+module.exports = { usersRouter }
