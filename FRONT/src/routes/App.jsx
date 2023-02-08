92b877a0330e3d32dbb277da764051bd46ee8ef7
davidmedev222 <davidmedev@gmail.com>
2023-02-08 20:09:27 -0300

    chore: add layout of the home page

--- FRONT/src/routes/App.jsx
+++ FRONT/src/routes/App.jsx
@@ -1,13 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Layout, Home, Login, Error } from '../pages/export'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1> HELLO WORLD</h1>} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export { App };
+export { App }
