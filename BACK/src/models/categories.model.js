9c145aac097a6e8a53403867185e85048ca4b22e
Jared Mejia <jaredmejia4440@gmail.com>
2023-02-08 20:15:55 -0600

    style: apply prettier to files

--- BACK/src/models/categories.model.js
+++ BACK/src/models/categories.model.js
@@ -1,18 +1,18 @@
 const { model, Schema } = require("mongoose");
 
 const categorySchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        required: true
-    },
-    updatedAt: {
-        type: Date,
-        required: true
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+  },
 });
 
 const categoryModel = model("categories", categorySchema);
--- BACK/src/models/users.model.js
+++ BACK/src/models/users.model.js
@@ -1,43 +1,43 @@
 const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: false
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        required: true
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        required: false
-    },
-    updatedAt: {
-        type: Date,
-        required: false
-    },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: false,
+  },
+  updatedAt: {
+    type: Date,
+    required: false,
+  },
 });
 
 const userModel = model("users", userSchema);
