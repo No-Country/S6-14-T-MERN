6a341dcda305f900640038722f8dce23e3411c35
Alex0711 <alex.senger@hotmail.com>
2023-02-10 22:57:44 -0300

    chore: save the payment

--- BACK/src/controllers/payments.controller.js
+++ BACK/src/controllers/payments.controller.js
@@ -1,51 +1,29 @@
-const userModel = require("./../models/users.model");
+const OrderModel = require("./../models/orders.model");
 const boom = require("@hapi/boom");
 
-const createPayment = async ({ userId, paymentData, orderId }) => {
-
+const createPayment = async ({ paymentData, orderId }) => {
   const newPayment = {
-    order: orderId,
+    paypalId: paymentData.id,
     amount: paymentData.purchase_units[0].amount.value,
     date: new Date(),
-    paypalId: paymentData.id,
     meta: JSON.stringify(paymentData),
   };
-  
-  const user = await userModel.findOne({ _id: userId })
-    .then(user => {
-      console.log({user})
-      user.payments.push(newPayment);
-      console.log("pago agregado");
-      console.log({user});
-      user.save();
-    })
-  return user;
-};
 
-const getOneProduct = async (req) => {
-  const { id } = req.params;
-
-  const data = await productModel.findOne({ _id: id });
-
-  return data;
-};
-
-const createProduct = async (req) => {
-  if (!req.file) {
-    throw boom.badRequest("File with the name productImg is required");
+  if (paymentData.status === "COMPLETED") {
+    const order = await OrderModel.findByIdAndUpdate(
+      orderId,
+      {
+        payment: newPayment,
+        status: {
+          name: "paid",
+        },
+      },
+      { new: true }
+    );
+    return order;
+  } else {
+    throw boom.notAcceptable("The payment was not completed")
   }
-  const imgPath = await uploadImg({
-    img: req.file,
-    folderName: "products",
-    id: 1,
-  });
-
-  return imgPath;
-};
-
-const deleteProduct = async (req, res, next) => {
-  const { imgUrl } = req.body;
-  await deleteImg(imgUrl);
 };
 
 module.exports = { createPayment };
