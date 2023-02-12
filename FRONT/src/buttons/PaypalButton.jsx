42d161549df8c65742dc7968654db1f13de40802
Alex0711 <alex.senger@hotmail.com>
2023-02-12 05:45:58 -0300

    fix: setIsRender is not a function in payPalButton

--- FRONT/src/buttons/PaypalButton.jsx
+++ FRONT/src/buttons/PaypalButton.jsx
@@ -7,52 +7,10 @@ const initialOptions = {
 };
 const PayPalButton = () => {
   const orderId = "63e5b15bb2e048edc3b6702c";
-
   const [orderDetails, setOrderDetails] = useState({});
-
-  /* useEffect(() => {
-    paypal
-      .Buttons({
-        createOrder: (data, actions) => {
-          return axios
-            .get(
-              `http://localhost:3000/api/v1/payments/create-payment/${orderId}`
-            )
-            .then((response) => {
-              return actions.order.create({
-                purchase_units: [
-                  {
-                    amount: {
-                      value: `${response.data.data}`,
-                    },
-                  },
-                ],
-              });
-            })
-            .catch((err) => {
-              setOrderDetails(err.response.data);
-            });
-        },
-        onApprove: (data, actions) => {
-          return actions.order.capture().then(function (paymentData) {
-            axios
-              .post(
-                `http://localhost:3000/api/v1/payments/success/${orderId}`,
-                { paymentData }
-              )
-              .then((res) => {
-                setIsRender(true);
-                setOrderDetails(res.data);
-              });
-          });
-        },
-      })
-      .render("#paypal-button-container");
-  }, []); */
 
   return (
     <>
-      {/* <div id="paypal-button-container"></div> */}
       <PayPalScriptProvider options={initialOptions}>
         <PayPalButtons
           createOrder={(data, actions) => {
@@ -83,7 +41,6 @@ const PayPalButton = () => {
                   { paymentData }
                 )
                 .then((res) => {
-                  setIsRender(true);
                   setOrderDetails(res.data);
                 });
             });
