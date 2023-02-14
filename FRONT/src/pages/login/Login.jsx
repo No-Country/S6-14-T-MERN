754feed5d53a8fcb47390ff125ba773578f73322
MichellMurallas <michellsms893@gmail.com>
2023-02-14 11:56:09 -0500

    chore: Login

--- FRONT/src/pages/login/Login.jsx
+++ FRONT/src/pages/login/Login.jsx
@@ -1,7 +1,12 @@
+import { SignIn } from "../../components/login/SignIn"
+import { SignUp } from "../../components/login/SignUp"
+
 const Login = () => {
   return (
     <main>
       <h2>LOGIN</h2>
+      <SignIn />
+      <SignUp />
     </main>
   )
 }
