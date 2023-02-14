be70d7126d43dd0a0bfb410f9dd4bf02596c69af
MichellMurallas <michellsms893@gmail.com>
2023-02-14 11:48:07 -0500

    chore: SignUp

--- FRONT/src/components/login/SignUp.jsx
+++ FRONT/src/components/login/SignUp.jsx
@@ -1,8 +1,29 @@
+import styled from 'styled-components'
+import { Submit } from './Variables'
+
+const Register = styled.div`
+width: 24rem;
+padding: 70px 0 0 90px;`
+
+const Ul = styled.ul`
+padding: 17px;`
+
 const SignUp = () => {
+
   return (
-    <form>
-      example
-    </form>
+    // <form>
+    //   example
+    // </form>
+    <Register>
+    <h2>Registrarse</h2>
+    <p>Crearse una cuenta tiene muchos beneficios</p>
+    <Ul>
+      <li>Guardar diseños</li>
+      <li>Seguiemiento de pedidos</li>
+      <li>Accede a descuentos</li>
+    </Ul>
+    <Submit className='registeri' type="submit" value="Registrarse" />
+  </Register>
   )
 }
 
