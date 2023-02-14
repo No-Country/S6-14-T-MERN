46c95200c0058fdc48aa567c07fbbe37e09ab1d1
Jared Mejia <jaredmejia4440@gmail.com>
2023-02-14 15:21:28 -0600

    build: config for deploy

--- BACK/src/server.js
+++ BACK/src/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const { connectDB } = require("./config/config");
 const globalErrorHandler = require("./controllers/error.controller");
 const { boomErrorHandler } = require("./middlewares/error.handler");
+const path = require("path");
 const cors = require("cors");
-const boom = require("@hapi/boom")
+const boom = require("@hapi/boom");
 
 const routerApi = require("./routes/index");
 
@@ -13,20 +14,21 @@ const app = express();
 
 connectDB();
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const whiteList = ["http://localhost:5173"]
+const whiteList = ["http://localhost:5173"];
 const options = {
   origin: (origin, cb) => {
     if (whiteList.includes(origin) || !origin) {
       cb(null, true);
     } else {
-      cb(boom.unauthorized("blocked by cors"))
+      cb(boom.unauthorized("blocked by cors"));
     }
-  }
-}
-app.use(cors(options))
+  },
+};
+app.use(cors(options));
 
 routerApi(app);
 
@@ -34,6 +36,12 @@ app.use(boomErrorHandler);
 app.use(globalErrorHandler);
 
 require("./utils/auth");
+
+app.use("/", (req, res, next) => {
+  const indexPath = path.join(__dirname, "..", "dist", "index.html");
+
+  res.status(200).sendFile(indexPath);
+});
 
 // Catch non-existing endpoints
 app.all("*", (req, res) => {
