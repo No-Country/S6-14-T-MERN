6e3393912df9983cda94eb16f8540a059dc9c951
MichellMurallas <michellsms893@gmail.com>
2023-02-14 12:06:06 -0500

    chore: variables-login

---
+++ FRONT/src/components/login/Variables.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Containt = styled.div`
+width: 100%;
+display:flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: center;`
+
+export const Submit = styled.input`
+border: none;
+border-radius: 100px;
+padding: 10px;
+margin: 20px;
+width: 10rem;
+color: white;
+background-color: #006FCF;`
