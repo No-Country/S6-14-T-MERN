2295ae31c7065cc562fb92e18c335ec288c3974a
CariBosio <caribosio72@gmail.com>
2023-02-16 17:30:01 -0300

    chore: add hero

--- FRONT/src/components/hero/Hero.jsx
+++ FRONT/src/components/hero/Hero.jsx
@@ -15,7 +15,6 @@ margin: 0 auto 5rem;
 ;
 
 h2{
- 
 width: 23.063rem;
 height: 10.875rem;
 left: 11rem;
@@ -30,7 +29,6 @@ margin:0 0 2rem 8rem;
 }
 
 h4{
-
 width: 35.813rem;
 height: 4.688rem;
 left: 11rem;
@@ -42,6 +40,24 @@ font-size: 1.75rem;
 line-height: 91.02%;
 margin:0 0 0 8rem;
 color: #FFFFFF;
+}
+
+button{
+width: 138px;
+height: 40px;
+left: 42.063rem;
+top: 410px;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+line-height: 91.02%;
+color: #FFFFFF;
+background: #0047FF;
+margin:2rem 0 0 8rem;
+border-radius:1rem;
+cursor: pointer;
+
 }
 
 img{
@@ -57,6 +73,7 @@ top: 6.375rem;
     <SectionHero>
       <h2>Custom Sport</h2>
       <h4>Las camisetas son 100% sublimadas, con diseños personalizados tal cual figuran en las imágenes</h4>
+      <button>Comprar</button>
       <img src='https://res.cloudinary.com/carina-bosio/image/upload/v1676571483/pngwing_1_f8zd1s.svg' />
     </SectionHero>
   )
