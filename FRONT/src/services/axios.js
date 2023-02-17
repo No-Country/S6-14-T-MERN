39a136b4c44a89bb51cb0aa370f2f2b4c30d56a4
Alex0711 <alex.senger@hotmail.com>
2023-02-17 02:06:49 -0300

    chore: add axios instance & API's endPoints

---
+++ FRONT/src/services/axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import Cookie from "js-cookie";
+
+const instance = () => {
+  return axios.create({
+    baseURL: 'http://localhost:3000/api/v1',
+    headers: {
+      Accept: '*/*',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${Cookie.get('token')}`,
+    },
+  });
+};
+export default instance;
