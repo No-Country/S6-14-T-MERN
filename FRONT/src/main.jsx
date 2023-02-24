883b4bfc164e1be28c9897d4b7a514683da58fad
davidmedev222 <davidmedev@gmail.com>
2023-02-24 18:49:17 -0300

    chore(web): add provider context of the cart

--- FRONT/src/main.jsx
+++ FRONT/src/main.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { GlobalStyles } from "./styles/GlobalStyles";
-import { App } from "./routes/App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GlobalStyles } from './styles/GlobalStyles'
+import { App } from './routes/App'
+import { CartProvider } from './context/cart/CartProvider'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <CartProvider>
+      <GlobalStyles />
+      <App />
+    </CartProvider>
   </React.StrictMode>
-);
+)
