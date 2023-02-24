d34487b1faa4f648a0a6a004fee70dd62b0c2dc6
Your Name <you@example.com>
2023-02-24 18:49:35 -0300

    chore: añadiendo estado

--- FRONT/src/components/orders/Orders.jsx
+++ FRONT/src/components/orders/Orders.jsx
@@ -1,62 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 
-const Orders = () => {
-
-
-  const Formulario = styled.form`
-    display: flex;
-  flex-direction: column;
-  width: 800px;
-  height: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 15px;
-
-  label {
-    margin-bottom: 5px;
-  }
-
-  input, textarea {
-    padding: 10px;
-    margin-bottom: 10px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    width: 100%; /* ajusta el ancho al 100% del contenedor */
-  }
-
-  textarea {
-    resize: vertical; /* permite al usuario cambiar la altura */
-  }
+const Formulario = styled.form`
+  display: flex;
+flex-direction: column;
+width: 800px;
+height: 600px;
+margin: 0 auto;
+padding: 20px;
+background-color: #fff;
+border: 1px solid #ccc;
+border-radius: 15px;
 `;
+
+const Label = styled.label` {
+margin-bottom: 5px;
+}
+`
+const Input =  styled.input` {
+padding: 10px;
+margin-bottom: 10px;
+border-radius: 5px;
+border: 1px solid #ccc;
+width: 100%; /* ajusta el ancho al 100% del contenedor */
+}
+`
+
+const Textarea = styled.textarea` {
+padding: 10px;
+margin-bottom: 10px;
+border-radius: 5px;
+border: 1px solid #ccc;
+width: 100%; /* ajusta el ancho al 100% del contenedor */
+}
+`
+
+// textarea {
+//   resize: vertical; /* permite al usuario cambiar la altura */
+// }
 
 const Titulo = styled.h2`
-  text-align: center;
-  margin-bottom: 5px;
+text-align: center;
+margin-bottom: 5px;
 `;
+const Orders = () => {
+
+  const order = {
+    shipping_full_name: '',
+    shipping_email: '',
+    shipping_phone: 0,
+    shipping_address: '',
+    shipping_comments: ''
+  }
+
+  const [inputs, setInputs] = useState(order)
+
+
+const handleChange = (e) => {
+  setInputs({
+    ...inputs,
+    [e.target.name] : e.target.value
+  })
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  
+  
+}
 
 
   return (
     <>
-    <Formulario>
+  {/* <input type="text"  name="shipping_full_name" value={inputs.shipping_full_name}  onChange={handleChange} /> */}
+    
+   <Formulario onSubmit={handleSubmit} >
     <Titulo>Datos de envio</Titulo>
-  <label htmlFor="nombre y apellido">Nombre:</label>
-  <input type="text" id="nombre" name="nombre" />
+    <Label htmlFor="shipping_full_name">Nombre:</Label>
+    <Input type="text"  name="shipping_full_name" value={inputs.shipping_full_name}  onChange={handleChange} />
 
-  <label htmlFor="email">Email:</label>
-  <input type="email" id="email" name="email" />
+    <Label htmlFor="shipping_email">Email:</Label>
+    <Input type="email" id="shipping_email" name="shipping_email" value={inputs.shipping_email}  onChange={handleChange} />
 
-  <label htmlFor="telefono">Teléfono:</label>
-  <input type="tel" id="telefono" name="telefono" />
+    <Label htmlFor="shipping_phone">Teléfono:</Label>
+    <Input type="tel" id="shipping_phone" name="shipping_phone" value={inputs.shipping_phone}  onChange={handleChange} />
 
-  <label htmlFor="direccion">Dirección de envío:</label>
-  <input type="text" id="direccion" name="direccion" />
+    <Label htmlFor="shipping_address">Dirección de envío:</Label>
+    <Input type="text" id="shipping_address" name="shipping_address" value={inputs.shipping_address}  onChange={handleChange} />
 
-  <label htmlFor="comentarios">Comentarios:</label>
-  <textarea id="comentarios" name="comentarios" rows="4" />
+    <Label htmlFor="shipping_comments">Comentarios:</Label>
+    <Textarea id="shipping_comments" name="shipping_comments" rows="4" value={inputs.shipping_comments} onChange={handleChange}/>
+    <button type='submit' > Enviar </button>
 </Formulario>
 </>
   )
