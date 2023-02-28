d814c2183854d9205f97eb098f3a13dcacbf2cb0
davidmedev222 <davidmedev@gmail.com>
2023-02-28 16:32:25 -0300

    chore(web): add new layout of the web

--- FRONT/src/styles/GlobalStyles.jsx
+++ FRONT/src/styles/GlobalStyles.jsx
@@ -66,8 +66,21 @@ const GlobalStyles = createGlobalStyle`
      font-family: var(--family-one);
      background-color: var(--background-one);
      display: grid;
-     grid-template-columns: 1fr;
-     grid-template-rows: auto 1fr auto;
+     grid-template: 
+     "header" auto
+     "main" 1fr
+     "footer" auto
+     / 1fr;
+   }
+
+   header {
+    grid-area: header;
+   }
+   main {
+    grid-area: main;
+   }
+   footer {
+    grid-area: footer;
    }
    
    img,
@@ -140,7 +153,6 @@ const GlobalStyles = createGlobalStyle`
    svg {
      width: 100%;
      display: block;
-     fill: currentColor;
    }
    
    @media (prefers-reduced-motion: reduce) {
