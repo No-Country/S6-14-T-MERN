abaa7f16c4aa3ab05fa2c0d6fa6649efb82ce9dd
CariBosio <caribosio72@gmail.com>
2023-03-01 00:18:30 -0300

    chore: ajuste estilos custom products

--- FRONT/src/components/customproducts/designs/Designs.jsx
+++ FRONT/src/components/customproducts/designs/Designs.jsx
@@ -12,28 +12,28 @@ z-index: -5;
 `
 
 const ImageDesign = styled.img`
-width: 120px;
-height:140px;
+width: clamp(1.875rem, -0.17rem + 10.23vw, 7.5rem); 
+height:clamp(2.188rem, -0.199rem + 11.93vw, 8.75rem);
 background: #D9D9D9;
 box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.25);
 border-radius: 8px;
-padding: 0.5rem;
+padding: clamp(0.188rem, 0.074rem + 0.57vw, 0.5rem);
 cursor: pointer;
 
 `
 
-const Designs = ({onChangeConfig}) => {
+const Designs = ({ onChangeConfig }) => {
   return (
     <SectionImagesDesign>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='1' onClick={() => onChangeConfig({ modelOption: 0 })}/>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='2' onClick={() => onChangeConfig({ modelOption: 1 })}/>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='3' onClick={() => onChangeConfig({ modelOption: 2 })}/>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='4' onClick={() => onChangeConfig({ modelOption: 3 })}/>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='5' onClick={() => onChangeConfig({ modelOption: 4 })}/>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='6' onClick={() => onChangeConfig({ modelOption: 5 })}/>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='7' onClick={() => onChangeConfig({ modelOption: 6 })}/>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='8' onClick={() => onChangeConfig({ modelOption: 7 })}/>
-      <ImageDesign src='https://res.cloudinary.com/carina-bosio/image/upload/v1676651263/frente_hdm9js.svg' alt='9' onClick={() => onChangeConfig({ modelOption: 8 })}/>
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192194/custom-sports/images/miniaturas/Miniatura1_rbjwab.svg' alt='1' onClick={() => onChangeConfig({ modelOption: 0 })} />
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192192/custom-sports/images/miniaturas/Miniatura2_fpldoq.svg' alt='2' onClick={() => onChangeConfig({ modelOption: 1 })} />
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192192/custom-sports/images/miniaturas/Miniatura3_xjoxog.svg' alt='3' onClick={() => onChangeConfig({ modelOption: 2 })} />
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192192/custom-sports/images/miniaturas/Miniatura4_mylxfw.svg' alt='4' onClick={() => onChangeConfig({ modelOption: 3 })} />
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192192/custom-sports/images/miniaturas/Miniatura5_glok3d.svg' alt='5' onClick={() => onChangeConfig({ modelOption: 4 })} />
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192192/custom-sports/images/miniaturas/Miniatura6_z3f9kt.svg' alt='6' onClick={() => onChangeConfig({ modelOption: 5 })} />
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192192/custom-sports/images/miniaturas/Miniatura7_vbwjjj.svg' alt='7' onClick={() => onChangeConfig({ modelOption: 6 })} />
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192193/custom-sports/images/miniaturas/Miniatura8_dhy41n.svg' alt='8' onClick={() => onChangeConfig({ modelOption: 7 })} />
+      <ImageDesign src='https://res.cloudinary.com/dwschrksm/image/upload/v1677192193/custom-sports/images/miniaturas/Miniatura9_p7exzl.svg' alt='9' onClick={() => onChangeConfig({ modelOption: 8 })} />
     </SectionImagesDesign>
   )
 }
