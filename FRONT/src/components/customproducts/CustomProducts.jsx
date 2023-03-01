b1ddc6a490789064dd6218c9999a1e3a8b216d9c
davidmedev222 <davidmedev@gmail.com>
2023-03-01 15:52:37 -0300

    chore(web): add interface responsive of the title and products

--- FRONT/src/components/customproducts/CustomProducts.jsx
+++ FRONT/src/components/customproducts/CustomProducts.jsx
@@ -8,167 +8,75 @@ import Modelo from './designs/Modelo'
 import Numero from './designs/Numero'
 import { useState } from 'react'
 
-const CustomProducts = () => {
-  const Interfaz = styled.div`
-    width: 100vw;
-    height: 100rem;
-    
-    hr {
-      height: 3px;
-      width: clamp(2.5rem, 0.795rem + 8.52vw, 7.188rem);
-      background-color: #ffffff;
-      margin-top: 20px;
-    }
-  `
-  const SectionHeader = styled.div`
-    ${'' /* width: 87.375rem; */}
-    ${'' /* height:300px; */}
-background-color: #090808;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 1rem 0;
-  `
-  const TituloCustomProduct = styled.h1`
-    ${'' /* width: 500px; */}
-    ${'' /* height: 48px; */}
-${'' /* font-family: 'Inter'; */}
-font-style: normal;
-    font-weight: 700;
-    font-size: clamp(1.563rem, 1.222rem + 1.7vw, 2.5rem);
-    line-height: 48px;
-    text-align: center;
-    letter-spacing: -0.6px;
-    color: #ffffff;
-  `
-  const SelectCustom = styled.div`
-    margin-top: 2.5rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 1.25rem;
-  `
-  const SelectProduct = styled.div`
-    width: clamp(3.125rem, 2.443rem + 3.41vw, 5rem);
-    ${'' /* height: 70px; */}
-    margin-bottom: 3rem;
-  `
+const SectionStyled = styled('section')`
+  color: var(--text-two);
+  background-color: var(--background-two);
+  padding: clamp(3rem, 3vw, 6rem) clamp(1rem,5vw,10rem);
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`
+const Heading = styled('h2')`
+  font-size: 1.75rem;
+  font-weight: bold;
+  text-align: center;
+`
+const ProductsUl = styled('ul')`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+`
+const ProductsLi = styled('li')`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  background-color: var(--background-one);
+  border-radius: 1rem;
+  overflow: hidden;
+`
 
-  const ImgSelectCamiseta = styled.img`
-    background: #ffffff;
-    border-radius: 8px 8px 0px 0px;
-    padding: 0.8rem;
-  `
-  //   const ImgSelect = styled.img`
-  //     ${
-  //       '' /* width: 50px;
-  // height: 65px; */
-  //     }
-  //     background: #FFFFFF;
-  //     border-radius: 8px 8px 0px 0px;
-  //     padding: 0.3rem;
-  //   `
-  const TextSelect = styled.div`
-    width: clamp(3.125rem, 2.443rem + 3.41vw, 5rem);
-    background-color: #0047ff;
-    border-radius: 0px 0px 8px 8px;
-    ${'' /* font-family: 'Inter'; */}
-    font-style: normal;
-    font-weight: 700;
-    font-size: clamp(0.625rem, 0.489rem + 0.68vw, 1rem);
-    line-height: 19px;
-    text-align: center;
-    letter-spacing: -0.6px;
-    color: #ffffff;
-  `
+const ProductsImage = styled('img')`
+  width: 5rem;
+  height: 5rem;
+  object-fit: contain;
+`
+const ProductsText = styled('span')`
+  text-align: center;
+  color: var(--text-one);
+  font-size: 0.875rem;
+  background-color: #0047ff;
+`
+const SectionDiseño = styled.div`
+background-color: #ffffff;
+display: flex;
+flex-direction: row;
+min-height: 500px;
+height: 40rem;
 
-  const SectionDiseño = styled.div`
-    background-color: #ffffff;
-    display: flex;
-    flex-direction: row;
-    min-height: 500px;
-    height: 40rem;
-    
-    ${'' /* width: 87.375rem; */}
-  `
-  const SectionResult = styled.div`
-    background-color: #d3d3d3;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 65%;
-  `
+${'' /* width: 87.375rem; */}
+`
+const SectionResult = styled.div`
+background-color: #d3d3d3;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 65%;
+`
 
-  // const Switch = styled.label`
-  //   width: 60px;
-  //   height: 25px;
-  //   position: relative;
-
-  //   .switch input {
-  //     display: none;
-  //   }
-
-  //   .slider {
-  //     position: absolute;
-  //     top: 0;
-  //     bottom: 0;
-  //     left: 0;
-  //     right: 0;
-  //     border-radius: 30px;
-  //     box-shadow: 0 0 0 2px #777, 0 0 4px #777;
-  //     cursor: pointer;
-  //     border: 4px solid transparent;
-  //     overflow: hidden;
-  //     transition: 0.2s;
-  //   }
-  //   .slider:before {
-  //     position: absolute;
-  //     content: "";
-  //     width: 100%;
-  //     height: 100%;
-  //     background-color: #777;
-  //     border-radius: 30px;
-  //     transform: translateX(-30px);
-  //     /*translateX(-(w-h))*/
-  //     transition: 0.2s;
-  //   }
-  //   input:checked + .slider:before {
-  //     transform: translateX(30px);
-  //     /*translateX(w-h)*/
-  //     background-color: #d9ff3d;
-  //   }
-
-  //   input:checked + .slider {
-  //     box-shadow: 0 0 0 2px #d9ff3d, 0 0 8px #d9ff3d;
-  //   }
-
-  //   .switch200 .slider:before {
-  //     width: 200%;
-  //     transform: translateX(-82px);
-  //     /*translateX(-(w-h))*/
-  //     background-color: #d9ff3d;
-  //   }
-
-  //   .switch200 input:checked + .slider:before {
-  //     background-color: #d9ff3d;
-  //   }
-
-  //   .switch200 input:checked + .slider {
-  //     box-shadow: 0 0 0 2px red, 0 0 8px #d9ff3d;
-  //   }
-  // `
-  const ImagesDefault = styled.div`
-    ${'' /* position: relative; */}
-    height: 100%;
-    width: 100%;
-    padding:clamp(0.313rem, -0.119rem + 2.16vw, 1.5rem) 0;
-    display: flex; 
-    flex-direction: column;
-  `
-  const Vistas = styled.div`
+const ImagesDefault = styled.div`
+${'' /* position: relative; */}
+height: 100%;
+width: 100%;
+padding:clamp(0.313rem, -0.119rem + 2.16vw, 1.5rem) 0;
+display: flex; 
+flex-direction: column;
+`
+const Vistas = styled.div`
 position: relative;
 display: flex;
 flex-direction: row;
@@ -179,98 +87,98 @@ width: 100%;
 padding: 0rem 0 2rem 0;
 
 `
-  const Position = styled.span`
-    ${'' /* font-family: 'Inter'; */}
-    font-style: normal;
-    font-weight: 700;
-    font-size: clamp(0.938rem, 0.369rem + 2.84vw, 2.5rem);
-    line-height: 48px;
-    text-align: center;
-    letter-spacing: -0.6px;
-    color: #000000;
-    width:100%;
-  `
-  const SectionOption = styled.div`
-  border-left: 1px solid #000000;
-  width: 35%;
-  display: inline-flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  `
+const Position = styled.span`
+font-style: normal;
+font-weight: 700;
+font-size: clamp(0.938rem, 0.369rem + 2.84vw, 2.5rem);
+line-height: 48px;
+text-align: center;
+letter-spacing: -0.6px;
+color: #000000;
+width:100%;
+`
+const SectionOption = styled.div`
+border-left: 1px solid #000000;
+width: 35%;
+display: inline-flex;
+flex-wrap: wrap;
+justify-content: center;
+align-items: center;
+`
 
-  const SectionSig = styled.div`
+const SectionSig = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
 background: #090808;
 
-    span {
-      color: #ffffff;
-    }
-  `
-  const PasosSig = styled.div`
-    display: flex;
-    justify-content: center;
-    gap: 1rem;
-    margin: 1.5rem 0;
-  `
-  const BtnSig = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 0.5rem;
-    margin: 1.5rem 0;
-  `
-  const SigText = styled.button`
-    font-style: normal;
-    font-weight: 700;
-    font-size: clamp(0.75rem, 0.477rem + 1.36vw, 1.5rem);
-    line-height: 29px;
-    text-align: center;
-    letter-spacing: -0.6px;
-    color: #ffffff;
-  `
-  const BtnFlecha = styled.button`
-    width: clamp(0.75rem, 0.341rem + 2.05vw, 1.875rem);
-    height: clamp(0.75rem, 0.341rem + 2.05vw, 1.875rem);
-    color: #ffffff;
-  `
-  const PasosNumSig = styled.div`
-    font-style: normal;
-    font-weight: 700;
-    font-size: clamp(1rem, 0.545rem + 2.27vw, 2.25rem);
-    line-height: 44px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    letter-spacing: -0.6px;
-    color: #000000;
-    background: #ffffff;
-    box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 50px;
-    width: 50px;
-    height: 50px;
+span {
+  color: #ffffff;
+}
+`
+const PasosSig = styled.div`
+display: flex;
+justify-content: center;
+gap: 1rem;
+margin: 1.5rem 0;
+`
+const BtnSig = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 0.5rem;
+margin: 1.5rem 0;
+`
+const SigText = styled.button`
+font-style: normal;
+font-weight: 700;
+font-size: clamp(0.75rem, 0.477rem + 1.36vw, 1.5rem);
+line-height: 29px;
+text-align: center;
+letter-spacing: -0.6px;
+color: #ffffff;
+`
+const BtnFlecha = styled.button`
+width: clamp(0.75rem, 0.341rem + 2.05vw, 1.875rem);
+height: clamp(0.75rem, 0.341rem + 2.05vw, 1.875rem);
+color: #ffffff;
+`
+const PasosNumSig = styled.div`
+font-style: normal;
+font-weight: 700;
+font-size: clamp(1rem, 0.545rem + 2.27vw, 2.25rem);
+line-height: 44px;
+display: flex;
+justify-content: center;
+align-items: center;
+text-align: center;
+letter-spacing: -0.6px;
+color: #000000;
+background: #ffffff;
+box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.25);
+border-radius: 50px;
+width: 50px;
+height: 50px;
 
-    ${
-      '' /* &:active{
-  background-color: #D9FF3D;
-  color: #000000;
+${
+  '' /* &:active{
+background-color: #D9FF3D;
+color: #000000;
 } */
 }
 
-    &:first-child {
-      background-color: #d9ff3d;
-      color: #000000;
-    }
-  `
-  const ContenedorCamiseta = styled.div`
-    position: absolute;
-    z-index: ${(props) => props.zIndex || 0};
-  `
+&:first-child {
+  background-color: #d9ff3d;
+  color: #000000;
+}
+`
+const ContenedorCamiseta = styled.div`
+  position: absolute;
+  z-index: ${(props) => props.zIndex || 0};
+`
 
+const CustomProducts = () => {
   const [config, setConfig] = useState({
     base: '#000',
     modelColor: '#fff',
@@ -281,7 +189,7 @@ background: #090808;
 
   const ordenDeCompra = config
 
-  localStorage.setItem('camisas', JSON.stringify({ ordenDeCompra }))
+  window.localStorage.setItem('camisas', JSON.stringify({ ordenDeCompra }))
 
   const baseColors = [
     '#DACD4A',
@@ -307,48 +215,48 @@ background: #090808;
   ]
 
   return (
-    <Interfaz>
-      <SectionHeader>
-        <TituloCustomProduct>Personalizar Camisetas</TituloCustomProduct>
-        <SelectCustom>
-          <SelectProduct>
-            <ImgSelectCamiseta
+    <main>
+      <SectionStyled>
+        <Heading>Personalizar Camisetas</Heading>
+        <ProductsUl>
+          <ProductsLi>
+            <ProductsImage
               src='https://res.cloudinary.com/carina-bosio/image/upload/v1676649279/camisetaBlanca_kknvhb.svg'
               alt='Camiseta Jugador'
             />
-            <TextSelect>Jugador</TextSelect>
-          </SelectProduct>
-          <SelectProduct>
-            <ImgSelectCamiseta
+            <ProductsText>Jugador</ProductsText>
+          </ProductsLi>
+          <ProductsLi>
+            <ProductsImage
               src='https://res.cloudinary.com/carina-bosio/image/upload/v1676649279/camisetaBlanca_kknvhb.svg'
               alt='Camiseta Arquero'
             />
-            <TextSelect>Arquero</TextSelect>
-          </SelectProduct>
-          {/* <SelectProduct>
-            <ImgSelect
+            <ProductsText>Arquero</ProductsText>
+          </ProductsLi>
+          <ProductsLi>
+            <ProductsImage
               src='https://res.cloudinary.com/carina-bosio/image/upload/v1676649290/pantalonBlanco_kaj7sd.svg'
               alt='Pantalón Jugador'
             />
-            <TextSelect>Jugador</TextSelect>
-          </SelectProduct> */}
-          {/* <SelectProduct>
-            <ImgSelect
+            <ProductsText>Jugador</ProductsText>
+          </ProductsLi>
+          <ProductsLi>
+            <ProductsImage
               src='https://res.cloudinary.com/carina-bosio/image/upload/v1676649290/pantalonBlanco_kaj7sd.svg'
               alt='Pantalón Arquero'
             />
-            <TextSelect>Arquero</TextSelect>
-          </SelectProduct> */}
-          {/* <SelectProduct>
-            <ImgSelect
+            <ProductsText>Arquero</ProductsText>
+          </ProductsLi>
+          <ProductsLi>
+            <ProductsImage
               src='https://res.cloudinary.com/carina-bosio/image/upload/v1676649302/mediasBlancas_vnh4kk.svg'
               alt='Medias'
             />
-            <TextSelect>Medias</TextSelect>
-          </SelectProduct> */}
-        </SelectCustom>
-      </SectionHeader>
-      <SectionDiseño>
+            <ProductsText>Medias</ProductsText>
+          </ProductsLi>
+        </ProductsUl>
+      </SectionStyled>
+      {/* <SectionDiseño>
         <SectionResult>
 
           <ImagesDefault>
@@ -419,8 +327,8 @@ background: #090808;
             </div>
           </div>
         </SectionOption>
-      </SectionDiseño>
-      <SectionSig>
+      </SectionDiseño> */}
+      {/* <SectionSig>
         <BtnSig>
           <a href=''>
             <SigText>Siguiente</SigText>
@@ -441,8 +349,8 @@ background: #090808;
           <hr />
           <PasosNumSig>3</PasosNumSig>
         </PasosSig>
-      </SectionSig>
-    </Interfaz>
+      </SectionSig> */}
+    </main>
   )
 }
 
