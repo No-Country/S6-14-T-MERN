ce4a475bf3cfd03ec8ea338016b9335300be15c8
davidmedev222 <davidmedev@gmail.com>
2023-03-02 00:25:44 -0300

    fix(web): change styles in the button and remove button

--- FRONT/src/components/orders/PlayersSet.jsx
+++ FRONT/src/components/orders/PlayersSet.jsx
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
-import styled from "styled-components";
-import addIcon from "./icons/addIcon.png";
-import { Player } from "./Player";
-import { OrderContext } from "../../context/order/OrderContext";
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import addIcon from './icons/addIcon.png'
+import { Player } from './Player'
+import { OrderContext } from '../../context/order/OrderContext'
 
 const Title = styled.h2`
   font-size: 24px;
   text-align: center;
-`;
+`
 
 const BoxSet = styled.div`
   margin: 10px auto;
@@ -15,19 +15,19 @@ const BoxSet = styled.div`
   width: 89vw;
   border-radius: 10px;
   padding: 1rem;
-`;
+`
 
 const ContainerAddIcon = styled.div`
   margin: 1.5rem auto 0;
   width: 41px;
   height: 41px;
-`;
+`
 
 const AddIcon = styled.img`
   cursor: pointer;
   filter: invert(73%) sepia(76%) saturate(4990%) hue-rotate(81deg)
     brightness(115%) contrast(128%);
-`;
+`
 
 const PlayersSet = () => {
   const { players, addPlayer } = useContext(OrderContext)
@@ -42,9 +42,8 @@ const PlayersSet = () => {
           <AddIcon src={addIcon} onClick={addPlayer} />
         </ContainerAddIcon>
       </BoxSet>
-      <button>Guardar jugadores</button>
     </>
-  );
-};
+  )
+}
 
-export { PlayersSet };
+export { PlayersSet }
