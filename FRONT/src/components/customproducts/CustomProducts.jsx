d0e1a82f745151404162481b1f8168546db78f57
davidmedev222 <davidmedev@gmail.com>
2023-03-01 21:57:07 -0300

    refactor(web): change styles for the responsive design of custom products

--- FRONT/src/components/customproducts/CustomProducts.jsx
+++ FRONT/src/components/customproducts/CustomProducts.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import './CustomProducts.css'
 import { Colors } from './colors/Colors'
 import { Numbers } from './numbers/Numbers'
 import { Designs } from './designs/Designs'
@@ -28,7 +27,7 @@ const ProductsUl = styled('ul')`
   flex-flow: row wrap;
   justify-content: center;
   align-items: center;
-  gap: 3rem;
+  gap: clamp(1.5rem, 3vw, 6rem);
 `
 const ProductsLi = styled('li')`
   display: flex;
@@ -38,7 +37,6 @@ const ProductsLi = styled('li')`
   border-radius: 1rem;
   overflow: hidden;
 `
-
 const ProductsImage = styled('img')`
   width: 5rem;
   height: 5rem;
@@ -50,134 +48,43 @@ const ProductsText = styled('span')`
   font-size: 0.875rem;
   background-color: #0047ff;
 `
-const SectionDiseño = styled.div`
-background-color: #ffffff;
-display: flex;
-flex-direction: row;
-height: 90vh;
+const WrapperCustomizer = styled('div')`
+  display: grid;
+  grid-template: 
+  "models options" 90vh
+  / 2fr 1fr;
 
-${'' /* width: 87.375rem; */}
+ @media screen and (max-width: 64rem) {
+    grid-template: 
+    "models" 70vw
+    "options" auto
+    / 1fr;
+ }
 `
-const SectionResult = styled.div`
-background-color: #d3d3d3;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-width: 65%;
-@media (max-width: 768px){
-  width: 100%;
-  height: 100vh;
-}
-`
+const WrapperModels = styled('div')`
+  background-color: var(--background-three);
+  grid-area: models;
+  position: relative;
+  overflow: hidden;
 
-const ImagesDefault = styled.div`
-${'' /* position: relative; */}
-height: 100%;
-width: 100%;
-padding:clamp(0.313rem, -0.119rem + 2.16vw, 1.5rem) 0;
-display: flex; 
-flex-direction: column;
+  & > div {
+    width: 100%;
+  }
 `
-const Vistas = styled.div`
-position: relative;
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-text-align: center;
-width: 100%;
-padding: 0rem 0 2rem 0;
-
+const WrapperOptions = styled('div')`
+  grid-area: options;
+  background-color: var(--background-two);
 `
-const Position = styled.span`
-font-style: normal;
-font-weight: 700;
-font-size: clamp(0.938rem, 0.369rem + 2.84vw, 2.5rem);
-line-height: 48px;
-text-align: center;
-letter-spacing: -0.6px;
-color: #000000;
-width:100%;
+const WrapperTabs = styled('div')`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--background-one);
 `
-const SectionOption = styled.div`
-border-left: 1px solid #000000;
-width: 35%;
-/* display: inline-flex;
-flex-wrap: wrap;
-justify-content: center;
-align-items: center; */
-@media (max-width: 768px){
-width: 100%;
-}
-`
-
-const SectionSig = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-background: #090808;
-
-span {
-  color: #ffffff;
-}
-`
-const PasosSig = styled.div`
-display: flex;
-justify-content: center;
-gap: 1rem;
-margin: 1.5rem 0;
-`
-const BtnSig = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-gap: 0.5rem;
-margin: 1.5rem 0;
-`
-const SigText = styled.button`
-font-style: normal;
-font-weight: 700;
-font-size: clamp(0.75rem, 0.477rem + 1.36vw, 1.5rem);
-line-height: 29px;
-text-align: center;
-letter-spacing: -0.6px;
-color: #ffffff;
-`
-const BtnFlecha = styled.button`
-width: clamp(0.75rem, 0.341rem + 2.05vw, 1.875rem);
-height: clamp(0.75rem, 0.341rem + 2.05vw, 1.875rem);
-color: #ffffff;
-`
-const PasosNumSig = styled.div`
-font-style: normal;
-font-weight: 700;
-font-size: clamp(1rem, 0.545rem + 2.27vw, 2.25rem);
-line-height: 44px;
-display: flex;
-justify-content: center;
-align-items: center;
-text-align: center;
-letter-spacing: -0.6px;
-color: #000000;
-background: #ffffff;
-box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.25);
-border-radius: 50px;
-width: 50px;
-height: 50px;
-
-${
-  '' /* &:active{
-background-color: #D9FF3D;
-color: #000000;
-} */
-}
-
-&:first-child {
-  background-color: #d9ff3d;
-  color: #000000;
-}
+const Tabs = styled('button')`
+  padding: clamp(.5rem, 1vw, 2rem) clamp(1rem, 2vw, 4rem);
+  ${({ isActive }) => isActive && 'color: var(--bg-component-one)'}
 `
 const ContenedorCamiseta = styled.div`
   position: absolute;
@@ -219,6 +126,21 @@ const CustomProducts = () => {
     '#263E32',
     '#874997'
   ]
+
+  const [options, updateOptions] = useState({
+    diseño: true,
+    colores: false,
+    numero: false
+  })
+
+  const handleOnOptions = (option) => {
+    updateOptions({
+      diseño: false,
+      colores: false,
+      numero: false,
+      [option]: true
+    })
+  }
 
   return (
     <main>
@@ -262,100 +184,42 @@ const CustomProducts = () => {
           </ProductsLi>
         </ProductsUl>
       </SectionStyled>
-      {/* <SectionDiseño>
-        <SectionResult>
-
-          <ImagesDefault>
-            <ContenedorCamiseta>
-              <Base fill={config.base} />
-            </ContenedorCamiseta>
-            <ContenedorCamiseta zIndex={10}>
-              <Modelo fill={config.modelColor} option={config.modelOption} />
-            </ContenedorCamiseta>
-            <ContenedorCamiseta zIndex={20}>
-              <Numero fill={config.numberColor} option={config.numberOption} />
-            </ContenedorCamiseta>
-            <ContenedorCamiseta zIndex={30}>
-              <img
-                style={{
-                  width: '64.9vw',
-                  position: 'relative',
-                  left: '0.05vw',
-                  top: '-0.082vw'
-
-                }}
-                src='/img/camiseta-sombra-doble.png'
-                alt=''
-              />
-            </ContenedorCamiseta>
-
-          </ImagesDefault>
-          <Vistas>
-
-            <Position>Frente</Position>
-
-            <Position>Espalda</Position>
-
-          </Vistas>
-        </SectionResult>
-        <SectionOption>
-          <div className='tabs'>
-            <div class='tab-container'>
-              <div id='tab3' class='tab'>
-                <a href='#tab3'>Números</a>
-                <div class='tab-content'>
-                  <Numbers
-                    baseColors={baseColors}
-                    onChangeConfig={(val) =>
-                      setConfig((prev) => ({ ...prev, ...val }))}
-                  />
-                </div>
-              </div>
-              <div id='tab2' class='tab'>
-                <a href='#tab2'>Colores</a>
-                <div class='tab-content'>
-                  <Colors
-                    baseColors={baseColors}
-                    onChangeConfig={(val) =>
-                      setConfig((prev) => ({ ...prev, ...val }))}
-                  />
-                </div>
-              </div>
-              <div id='tab1' class='tab'>
-                <a href='#tab1'>Diseño</a>
-                <div class='tab-content'>
-                  <Designs
-                    onChangeConfig={(val) =>
-                      setConfig((prev) => ({ ...prev, ...val }))}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </SectionOption>
-      </SectionDiseño> */}
-      {/* <SectionSig>
-        <BtnSig>
-          <a href=''>
-            <SigText>Siguiente</SigText>
-          </a>
-          <a href=''>
-            <BtnFlecha>
-              <img
-                src='https://res.cloudinary.com/carina-bosio/image/upload/v1676755392/Flecha_vnyn9c.svg'
-                alt='Flecha'
-              />
-            </BtnFlecha>
-          </a>
-        </BtnSig>
-        <PasosSig>
-          <PasosNumSig>1</PasosNumSig>
-          <hr />
-          <PasosNumSig>2</PasosNumSig>
-          <hr />
-          <PasosNumSig>3</PasosNumSig>
-        </PasosSig>
-      </SectionSig> */}
+      <WrapperCustomizer>
+        <WrapperModels>
+          <ContenedorCamiseta>
+            <Base fill={config.base} />
+          </ContenedorCamiseta>
+          <ContenedorCamiseta zIndex={10}>
+            <Modelo fill={config.modelColor} option={config.modelOption} />
+          </ContenedorCamiseta>
+          <ContenedorCamiseta zIndex={20}>
+            <Numero fill={config.numberColor} option={config.numberOption} />
+          </ContenedorCamiseta>
+        </WrapperModels>
+        <WrapperOptions>
+          <WrapperTabs>
+            <Tabs isActive={options.diseño} onClick={() => handleOnOptions('diseño')}>Diseño</Tabs>
+            <Tabs isActive={options.colores} onClick={() => handleOnOptions('colores')}>Colores</Tabs>
+            <Tabs isActive={options.numero} onClick={() => handleOnOptions('numero')}>Números</Tabs>
+          </WrapperTabs>
+          {options.diseño &&
+            <Designs
+              onChangeConfig={(val) => setConfig((prev) => ({ ...prev, ...val }))}
+            />}
+          {options.colores &&
+            <Colors
+              baseColors={baseColors}
+              onChangeConfig={(val) =>
+                setConfig((prev) => ({ ...prev, ...val }))}
+            />}
+          {options.numero &&
+            <Numbers
+              baseColors={baseColors}
+              onChangeConfig={(val) =>
+                setConfig((prev) => ({ ...prev, ...val }))}
+            />}
+        </WrapperOptions>
+      </WrapperCustomizer>
     </main>
   )
 }
