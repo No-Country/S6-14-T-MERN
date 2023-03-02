f3ff76a5bcca47eaac5003fd6b8ac92746556645
davidmedev222 <davidmedev@gmail.com>
2023-03-02 01:03:04 -0300

    feat(web): add component for the construction pages

---
+++ FRONT/src/pages/mayorista/MayoristaPage.jsx
@@ -0,0 +1,11 @@
+import { Construction } from '../../components/construction/Construction'
+
+const MayoristaPage = () => {
+  return (
+    <main>
+      <Construction />
+    </main>
+  )
+}
+
+export { MayoristaPage }
