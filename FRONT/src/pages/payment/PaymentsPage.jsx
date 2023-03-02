a6b577883eacbd90f4b1c6d5c8edffcd683d357c
davidmedev222 <davidmedev@gmail.com>
2023-03-01 22:47:52 -0300

    chore(web): add steps to buy in the order and payment pages

--- FRONT/src/pages/payment/PaymentsPage.jsx
+++ FRONT/src/pages/payment/PaymentsPage.jsx
@@ -1,11 +1,13 @@
 import TotalCompra from '../../components/orders/TotalCompra'
 import PayPalButton from '../../buttons/PaypalButton'
+import { Steps } from '../../components/steps/Steps'
 
 const PaymentsPage = () => {
   return (
     <main>
       <TotalCompra />
       <PayPalButton />
+      <Steps two three />
     </main>
   )
 }
